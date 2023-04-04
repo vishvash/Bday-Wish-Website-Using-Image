0631619f1492df845661ea2598bad7382821fc24
Yogesh R <82885288+itsyogieu@users.noreply.github.com>
2023-04-04 08:02:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -287,10 +287,10 @@ function reveal() {
 
   var w, h;
   if (window.innerWidth >= 1000) {
-    w = 295;h = 185;
+    w = 6720;h = 480;
   } else
   {
-    w = 255;h = 155;
+    w = 6720;h = 480;
   }
 
   var ifrm = document.createElement("iframe");
